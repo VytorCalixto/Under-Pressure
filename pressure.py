21d01b4feabf826e7690b960f9fb40a4af59503d
VytorCalixto <vytorcalixto@gmail.com>
2015-10-20 15:43:12 -0200

    Iniciado implementação
    Lendo e fazendo parse das opções vindas do terminal

---
+++ pressure.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+import argparse
+
+def parse_args():
+    # parser para os argumentos
+    parser = argparse.ArgumentParser(description="UnderPressure: Comprime textos usando LZW", prog="pressure")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-c", "--compress", help="comprime o texto dado", action="store_true")
+    group.add_argument("-x", "--extract", help="decomprime o texto dado", action="store_true")
+    parser.add_argument("-b", "--base64", help="para textos não ASCII, utiliza essa opção para convertê-los para base64 antes", action="store_true")
+    #parser.add_argument("file", type=file, help="arquivo de texto para ser comprimido")
+    return parser.parse_args()
+
+
+def main(args):
+    base64 = False
+    if args.compress:
+        print "Compress"
+    if args.extract:
+        print "Extract"
+    if args.base64:
+        print "Base64"
+        base64 = True
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
