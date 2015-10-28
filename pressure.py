72a76931aff9ee59d5ea7625c13930dd0be07615
VytorCalixto <vytorcalixto@gmail.com>
2015-10-28 21:50:46 -0200

    LoremIpsum de 10 parágrafos e base64 funcionando

--- pressure.py
+++ pressure.py
@@ -1,46 +1,67 @@
 # -*- coding:utf-8 -*-
 import argparse
+import base64
+import logging
 
 def parse_args():
     # parser para os argumentos
     parser = argparse.ArgumentParser(description="UnderPressure: Comprime textos usando LZW", prog="pressure")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument("-c", "--compress", help="comprime o texto dado", action="store_true")
-    group.add_argument("-x", "--extract", help="decomprime o texto dado", action="store_true")
-    parser.add_argument("-b", "--base64", help="para textos não ASCII, utiliza essa opção para convertê-los para base64 antes", action="store_true")
-    parser.add_argument("input", type=file, help="arquivo de texto para ser comprimido")
-    parser.add_argument("output", help="nome do arquivo de saida")
+    group.add_argument("-c", "--compress", help="Comprime o texto dado", action="store_true")
+    group.add_argument("-x", "--extract", help="Decomprime o texto dado", action="store_true")
+    parser.add_argument("-b", "--base64", help="Para textos não ASCII, utiliza essa opção para convertê-los para base64 antes", action="store_true")
+    parser.add_argument("-v", "--verbose", help="Executa de forma verbosa", action="store_true")
+    parser.add_argument("input", type=file, help="Arquivo de texto para ser comprimido")
+    parser.add_argument("output", help="Nome do arquivo de saida")
     return parser.parse_args()
 
-def compress(filename, base64, output):
-    # tem que ler o arquivo aqui
-    text = filename
+def compress(inputFile, base64, output):
+    verbose("Comprimindo arquivo", inputFile)
+    text = inputFile.read()
     if(base64):
         text = encodeBase64(text)
     writeFile(output, text)
 
-def extract(filename, base64, output):
-    text = filename
+def extract(inputFile, base64, output):
+    text = inputFile.read()
     # extrai e DEPOIS tira do base64
     if(base64):
         text = decodeBase64(text)
-    return text
+    writeFile(output, text)
 
 def encodeBase64(text):
-    print "Encoding in base64"
+    verbose("Codificando em Base64")
+    encoded = base64.b64encode(text)
+    verbose("Codificado!")
+    return encoded
 
 def decodeBase64(text):
-    print "Decoding in base64"
+    verbose("Decodificando de Base64")
+    decoded = base64.b64decode(text)
+    verbose("Decodificado!")
+    return decoded
 
 def writeFile(filename, text):
-    print "escrevendo arquivo..."
+    verbose("Escrevendo arquivo de saída")
+    f = open(filename, "w")
+    f.write(text)
+    verbose("Feito")
 
 def main(args):
     if args.compress:
         compress(args.input, args.base64, args.output)
-    if args.extract:
+    elif args.extract:
         extract(args.input, args.base64, args.output)
+    else:
+        print "Nenhuma opção foi escolhida. Saindo."
 
 if __name__ == "__main__":
     args = parse_args()
+    if(args.verbose):
+        def verbose(*args):
+            for arg in args:
+                print arg,
+            print
+    else:
+        verbose = lambda *a: None
     main(args)
